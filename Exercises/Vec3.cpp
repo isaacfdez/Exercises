924d79d93cf040b0b62cc0832efb84aa22e432d9
isaac fernandez fontao <isaacfdez94@gmail.com>
2020-10-22 21:18:28 +0200

    Fixed with observations from the teacher during class

--- Exercises/Vec3.cpp
+++ Exercises/Vec3.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 
-Vec3& Vec3::Normalize()
+const Vec3 Vec3::Normalize()
 {
 	float magnitud = sqrt((x * x) + (y * y) + (z * z));
 	return *new Vec3(x / magnitud, y / magnitud, z / magnitud);
 }
 
-float Vec3::distanceTo(const Vec3& anotherVec3)
+const float Vec3::distanceTo(const Vec3& anotherVec3)
 {
 	return sqrt(((x - anotherVec3.x) * (x - anotherVec3.x)) + ((y - anotherVec3.y) * (y - anotherVec3.y)) + ((z - anotherVec3.z) * (z - anotherVec3.z)));
 }
 
-float Vec3::dot_product(const Vec3& anotherVec3)
+const float Vec3::dot_product(const Vec3& anotherVec3)
 {
-	return (x*anotherVec3.x)+ (y * anotherVec3.y) + (z * anotherVec3.z);
+	return (x * anotherVec3.x) + (y * anotherVec3.y) + (z * anotherVec3.z);
 }
 
-Vec3& Vec3::cross_product(const Vec3& anotherVec3)
+const Vec3 Vec3::cross_product(const Vec3& anotherVec3)
 {
-	return *new Vec3(((y*anotherVec3.z)-(z*anotherVec3.y)), ((z * anotherVec3.x) - (x * anotherVec3.z)), ((x * anotherVec3.y) - (y * anotherVec3.x)));
+	return *new Vec3(((y * anotherVec3.z)-(z * anotherVec3.y)), ((z * anotherVec3.x) - (x * anotherVec3.z)), ((x * anotherVec3.y) - (y * anotherVec3.x)));
 }
 
-float Vec3::angle_between(const Vec3& anotherVec3)
+const float Vec3::angle_between(const Vec3& anotherVec3)
 {
 	return acos(dot_product(anotherVec3) / sqrt((x * x + y * y + z * z) * (anotherVec3.x * anotherVec3.x + anotherVec3.y * anotherVec3.y + anotherVec3.z * anotherVec3.z)));
 }
--- Exercises/Vec3.h
+++ Exercises/Vec3.h
@@ -1,25 +1,26 @@
 #pragma once
 #include <iostream>
 
-using namespace std;
-
 class Vec3
 {
 public:
 
-	inline Vec3() : x(0), y(0), z(0) {}
-	inline Vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
-	inline Vec3(const Vec3& anotherVec3) { x = anotherVec3.x; y = anotherVec3.y; z = anotherVec3.z; }
-	inline ~Vec3() {}
+	Vec3() : x(0), y(0), z(0) {}
+	Vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+	Vec3(const Vec3& anotherVec3) : x(anotherVec3.x), y(anotherVec3.y), z(anotherVec3.z) {}
 
-	inline Vec3& operator+(const Vec3& anotherVec3) { return *new Vec3(x+anotherVec3.x, y + anotherVec3.y, z + anotherVec3.z); }
-	inline friend ostream& operator<<(ostream& os, const Vec3& anotherVec3) { return os << "(" << anotherVec3.x << ", " << anotherVec3.y << ", " << anotherVec3.z << ")"; }
+	Vec3& operator+(const Vec3& anotherVec3) { x = x + anotherVec3.x; y = y + anotherVec3.y; z = z + anotherVec3.z; return *this; }
+	friend ostream& operator<<(ostream& os, const Vec3& anotherVec3) { return os << "(" << anotherVec3.x << ", " << anotherVec3.y << ", " << anotherVec3.z << ")"; }
 
-	Vec3& Normalize();
-	float distanceTo(const Vec3& anotherVec3);
-	float dot_product(const Vec3& anotherVec3);
-	Vec3& cross_product(const Vec3& anotherVec3);
-	float angle_between(const Vec3& anotherVec3);
+	const Vec3 Normalize();
+	const float distanceTo(const Vec3& anotherVec3);
+	const float dot_product(const Vec3& anotherVec3);
+	const Vec3 cross_product(const Vec3& anotherVec3);
+	const float angle_between(const Vec3& anotherVec3);
+
+	float getX() { return x; }
+	float getY() { return y; }
+	float getZ() { return z; }
 
 private:
 	float x, y, z;
