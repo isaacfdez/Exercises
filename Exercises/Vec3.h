5ac82d53fed94f335fd35b99faafb3a78420dd82
isaac fernandez fontao <isaacfdez94@gmail.com>
2020-10-22 10:47:46 +0200

    Creation of Vec3 Class methods and variables

--- Exercises/Vec3.h
+++ Exercises/Vec3.h
@@ -1,5 +1,24 @@
 #pragma once
+
+using namespace std;
+
 class Vec3
 {
+public:
+
+	Vec3();
+	Vec3(int x, int y, int z);
+	Vec3(const Vec3& anotherVec3);
+	~Vec3();
+
+	void Normalize();
+	Vec3& distanceTo(Vec3& anotherVec3);
+	Vec3& dot_product(Vec3& anotherVec3);
+	Vec3& cross_product(Vec3& anotherVec3);
+	Vec3& angle_between(Vec3& anotherVec3);
+
+private:
+	int* elementsInVector;
+	float* vectorNormalized;
 };
 
