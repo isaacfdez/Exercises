2cb922899f686d8bef5b9711fe9265763748e8c2
isaac fernandez fontao <isaacfdez94@gmail.com>
2020-10-26 16:19:13 +0100

    Creation of functions for String class

--- Exercises/String.cpp
+++ Exercises/String.cpp
@@ -1,1 +1,36 @@
 #include "String.h"
+
+String::String()
+{
+}
+
+String::String(const char* newSentence)
+{
+}
+
+String::String(const String& anotherString)
+{
+}
+
+String::String(String&& anotherString)
+{
+}
+
+const int String::length()
+{
+	return 0;
+}
+
+void String::clear()
+{
+}
+
+String String::operator+(const String& anotherString)
+{
+	return String();
+}
+
+String String::operator==(const String& anotherString)
+{
+	return String();
+}
--- Exercises/String.h
+++ Exercises/String.h
@@ -3,5 +3,19 @@
 
 class String
 {
+public:
+	String();
+	String(const char* newSentence);
+	String(const String& anotherString);
+	String(String&& anotherString);
+
+	const int length();
+	void clear();
+
+	String operator+(const String& anotherString);
+	String operator==(const String& anotherString);
+
+private:
+	char* sentence;
 };
 
